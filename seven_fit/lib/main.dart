9317077bdafea1fe80a5903ae20e1f8073b39828
Jhones Silva dos Santos <jhonessds@gmail.com>
2019-12-27 11:47:13 -0300

    alteração na arquitetura do projeto

--- seven_fit/lib/main.dart
+++ seven_fit/lib/main.dart
@@ -1,19 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:seven_fit/screens/login_screen.dart';
+import 'package:seven_fit/app/app_module.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "SevenFit",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Color(0xffff3a5a),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
-    );
-  }
-}
+void main() => runApp(AppModule());
